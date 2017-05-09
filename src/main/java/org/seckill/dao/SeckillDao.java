e1ad95998259ee7054d4446345ae2b8cd2c0f81c
newWSmile <wyj950412>
2017-05-09 22:09:24 +0800

    mybatis 实现dao接口和mapper.xml实现sql语句

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -26,10 +26,10 @@ public interface SeckillDao {
 
     /**
      * 根据偏移量查询秒杀商品列表
-     * @param offet
+     * @param offset
      * @param limit
      * @return
      */
-    List<Seckill> queryAll(int offet,int limit);
+    List<Seckill> queryAll(int offset,int limit);
 
 }
